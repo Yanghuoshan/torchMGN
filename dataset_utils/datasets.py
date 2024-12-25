9f68b497529aa8bc41aa80c5f62c915438c239ae
yanghuoshan <yanghuoshan2021@163.com>
2024-12-26 00:41:02 +0800

    hdf5 dataset

--- dataset_utils/datasets.py
+++ dataset_utils/datasets.py
@@ -14,6 +14,8 @@ sys.path.append('../')
 import torch.utils
 import torch.utils.data
 
+import h5pickle as h5py
+
 from tqdm import trange
 
 from model_utils.common import build_graph_HyperEl, build_graph_Cloth
@@ -250,6 +252,80 @@ class Cloth_trajectory_dataset(torch.utils.data.Dataset):
         return [graph, target, d['node_type']]
 
 
+
+class Cloth_single_dataset_hdf5(torch.utils.data.Dataset):
+    def __init__(self, path, is_data_graph = False):
+        self.path = path
+        self.meta = json.loads(open(os.path.join(path, 'metadata.json')).read())
+        self.files = self.meta['files']
+        self.num_samples = sum(self.files[f] - 2 for f in self.files)
+
+        self.hdf5_dataset = h5py.File(os.path.join(path, 'dataset.h5'), 'r')
+
+        if is_data_graph:
+            self.return_item = self.return_graph
+        else:
+            self.return_item = self.return_dict
+
+    @property
+    def avg_nodes_per_sample(self):
+        total_nodes = 0
+        total_samples = 0
+        for fname, num_steps in self.files.items():
+            data = self.hdf5_dataset[fname]
+            total_nodes += data['mesh_pos'][:].shape[1] * (num_steps - 2)
+            total_samples += (num_steps - 2)
+
+        return total_nodes / total_samples
+
+
+    def idx_to_file(self, sample_id):
+        for fname, num_steps in self.files.items():
+            if sample_id < (num_steps - 2): return fname, sample_id
+            else: sample_id -= (num_steps - 2)
+        raise IndexError()
+
+    def __len__(self): return self.num_samples
+
+    def __getitem__(self, idx : int) -> dict:
+        fname, sid = self.idx_to_file(idx)
+        data = self.hdf5_dataset[fname]
+
+        return self.return_item(data, sid)
+    
+    def return_dict(self, data, sid):
+        return dict(
+            cells=torch.LongTensor(data['cells'][sid, ...]),
+            node_type=torch.LongTensor(data['node_type'][sid, ...]),
+            mesh_pos=torch.Tensor(data['mesh_pos'][sid, ...]),
+            world_pos=torch.Tensor(data['world_pos'][sid + 1, ...]),
+            prev_world_pos=torch.Tensor(data['world_pos'][sid, ...]),
+            target_world_pos=torch.Tensor(data['world_pos'][sid + 2, ...])
+        )
+
+    def return_graph(self, data, sid):
+        d = dict(
+            cells=torch.LongTensor(data['cells'][sid, ...]),
+            node_type=torch.LongTensor(data['node_type'][sid, ...]),
+            mesh_pos=torch.Tensor(data['mesh_pos'][sid, ...]),
+            world_pos=torch.Tensor(data['world_pos'][sid + 1, ...]),
+            prev_world_pos=torch.Tensor(data['world_pos'][sid, ...]),
+            target_world_pos=torch.Tensor(data['world_pos'][sid + 2, ...])
+        )
+        graph = build_graph_Cloth(d)
+
+        world_pos = d['world_pos']
+        prev_world_pos = d['prev_world_pos']
+        target_world_pos = d['target_world_pos']
+
+        cur_position = world_pos
+        prev_position = prev_world_pos
+        target_position = target_world_pos
+        target = target_position - 2 * cur_position + prev_position
+
+        return [graph, target, d['node_type']]
+
+
 def my_collate_fn(batch): # cumstom collate fn
         # batch [data1, data2...]
         return batch
@@ -277,7 +353,7 @@ def get_dataloader(path,
     ds = Datasets(path, is_data_graph)
     if prefetch == 0:
         return torch.utils.data.DataLoader(ds, batch_size=1, shuffle = shuffle, collate_fn=my_collate_fn)
-    return torch.utils.data.DataLoader(ds, batch_size=1, shuffle = shuffle, prefetch_factor=prefetch, num_workers=4, pin_memory=True, collate_fn=my_collate_fn)
+    return torch.utils.data.DataLoader(ds, batch_size=1, shuffle = shuffle, prefetch_factor=prefetch, num_workers=8, pin_memory=True, collate_fn=my_collate_fn)
 
 
 def get_trajectory_dataloader(path,
@@ -296,18 +372,40 @@ def get_trajectory_dataloader(path,
     ds = Datasets(path, is_data_graph, trajectory_index)
     if prefetch == 0:
         return torch.utils.data.DataLoader(ds, batch_size=1, shuffle = shuffle, collate_fn=my_collate_fn)
-    return torch.utils.data.DataLoader(ds, batch_size=1, shuffle = shuffle, prefetch_factor=prefetch, num_workers=4, pin_memory=True, collate_fn=my_collate_fn)
+    return torch.utils.data.DataLoader(ds, batch_size=1, shuffle = shuffle, prefetch_factor=prefetch, num_workers=8, pin_memory=True, collate_fn=my_collate_fn)
+
+
+def get_dataloader_hdf5(path, 
+                   model = "Cloth",
+                   split = "train",
+                   shuffle = True,
+                   prefetch = 0,
+                   is_data_graph = False):
+    """
+    根据不同的模型使用不同的数据类
+    """
+
+    path = os.path.join(path,split)
+    if model == "Cloth":
+        Datasets = Cloth_single_dataset_hdf5
+    else:
+        raise ValueError("The dataset type doesn't exist.")
+    
+    ds = Datasets(path, is_data_graph)
+    if prefetch == 0:
+        return torch.utils.data.DataLoader(ds, batch_size=1, shuffle = shuffle, collate_fn=my_collate_fn)
+    return torch.utils.data.DataLoader(ds, batch_size=1, shuffle = shuffle, prefetch_factor=prefetch, num_workers=8, pin_memory=True, collate_fn=my_collate_fn)
 
 
 if __name__ == "__main__":
     # ds = deforming_datasets("D:\project_summary\Graduation Project\\tmp\datasets_np\deforming_plate\\train")
     # ds = cloth_datasets("D:\project_summary\Graduation Project\\tmp\datasets_np\\flag_simple\\train")
     # ds = flow_datasets("D:\project_summary\Graduation Project\\tmp\datasets_np\\cylinder_flow\\train")
-    dl = get_dataloader("D:\project_summary\Graduation Project\\tmp\datasets_np\\flag_simple",model="Cloth",split="train",prefetch=0,is_data_graph=False)
+    dl = get_dataloader_hdf5("D:\project_summary\Graduation Project\\tmp\datasets_hdf5\\flag_simple",model="Cloth",split="train",prefetch=0,is_data_graph=False)
     dl = iter(dl)
     start_time = time.time()
-    
-    print(next(dl))
+    for _ in range(100):
+        next(dl)
     end_time = time.time()
-    execution_time = (end_time - start_time)/10
+    execution_time = (end_time - start_time)/100
     print(f"运行时间: {execution_time} 秒")
---
+++ dataset_utils/tfrecord2hdf5.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+from absl import app
+from absl import flags
+import sys
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+import numpy as np
+import json
+import functools
+import h5py
+
+""" 
+The data format is as follows:
+
+material_dataset/
+├── metadata.json -> dataset features
+├── train/
+│   ├── metadata.json
+│   └── dataset.h5
+├── valid/
+│   ├── metadata.json -> timesteps per ex
+│   └── dataset.h5
+├── test/
+│   ├── metadata.json
+│   └── dataset.h5
+└── ...
+
+train.h5 = {ex0:dict, ex1:dict, ...}
+
+"""
+
+tf._logging
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('in_dir', '../tmp/datasets/', 'Input datasets directory')
+flags.DEFINE_string('out_dir', '../tmp/datasets_hdf5/','Output numpy datasets directory')
+flags.DEFINE_string('dataset_name', 'flag_simple', '')
+flags.DEFINE_string('split', 'train', '')
+flags.DEFINE_boolean('debug', False, 'Enable debugging output')
+
+def _parse(proto, meta):
+    """Parses a trajectory from tf.Example."""
+    feature_lists = {k: tf.io.VarLenFeature(tf.string)
+                    for k in meta['field_names']}
+    features = tf.io.parse_single_example(proto, feature_lists)
+    out = {}
+    for key, field in meta['features'].items():
+        data = tf.io.decode_raw(features[key].values, getattr(tf, field['dtype']))
+        data = tf.reshape(data, field['shape'])
+        if field['type'] == 'static':
+            data = tf.tile(data, [meta['trajectory_length'], 1, 1])
+        elif field['type'] == 'dynamic_varlen':
+            length = tf.io.decode_raw(features['length_'+key].values, tf.int32)
+            length = tf.reshape(length, [-1])
+            data = tf.RaggedTensor.from_row_lengths(data, row_lengths=length)
+        elif field['type'] != 'dynamic':
+            raise ValueError('invalid data format')
+        out[key] = data
+    return out
+
+
+def load_dataset(dataset_name, split):
+    with open(os.path.join(dataset_name, 'metadata.json'), 'r') as fp:
+        meta = json.loads(fp.read())
+    ds = tf.data.TFRecordDataset(os.path.join(dataset_name, f'{split}.tfrecord'))
+    ds = ds.map(functools.partial(_parse, meta=meta), num_parallel_calls=8)
+    ds = ds.prefetch(1)
+    return ds
+
+
+def convert_to_npdict(example) -> dict:
+    result = {}
+    # example.features.feature is the dictionary
+    for key, feature in example.features.feature.items():
+        # The values are the Feature objects which contain a `kind` which contains:
+        # one of three fields: bytes_list, float_list, int64_list
+
+        kind = feature.WhichOneof('kind')
+        result[key] = np.array(getattr(feature, kind).value)
+
+    return result
+
+def main(argv):
+    in_dir = FLAGS.in_dir
+    out_dir = FLAGS.out_dir
+    dataset_name = FLAGS.dataset_name
+    split = FLAGS.split
+
+    in_path = os.path.join(in_dir, dataset_name)
+    out_path = os.path.join(out_dir, dataset_name, split)
+
+    if not os.path.exists(in_path):
+        raise FileNotFoundError(f"in_path {in_path} doesn't exixt")
+
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+
+    print("in_path:", in_path)
+    print("out_path:", out_path)
+
+    ds = load_dataset(in_path, split)
+    first = True
+
+    outfile = os.path.join(out_path, 'metadata.json')
+    outh5 = os.path.join(out_path, f'dataset.h5')
+    with h5py.File(outh5, 'w') as h5f:
+        with open(outfile, 'w') as meta_file:
+            print('{', file=meta_file)
+            print('    "files": {', file=meta_file)
+            for i, record in enumerate(ds):
+                print(f'==== Record {i} ====')
+                for k, v in record.items():
+                    print(k, v.shape)
+                    h5f.create_dataset(f"ex{i}/{k}", data=v)
+
+                ns = record['cells'].shape[0]
+
+                if first: first = False
+                else: print(',', file=meta_file)
+                
+                # np.savez_compressed(os.path.join(out_path, f'ex{i}.npz'), **record)
+                print(f'        "ex{i}": {ns}', file=meta_file, end='')
+
+            print('', file=meta_file)
+            print('    }', file=meta_file)
+            print('}', file=meta_file)
+
+if __name__ == "__main__":
+    app.run(main)
+
+
+
